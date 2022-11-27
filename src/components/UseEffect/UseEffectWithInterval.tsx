f58c5c93b719b66b5e69555c21d39cfaeed0e1d4
rybaaa <bolshayaryba22@gmail.com>
2022-11-27 16:51:10 +0300

    refactoring clock

--- src/components/UseEffect/UseEffectWithInterval.tsx
+++ src/components/UseEffect/UseEffectWithInterval.tsx
@@ -1,17 +1,22 @@
 import React, {useEffect, useState} from 'react';
 
+const transfer = (number:number) => {
+    return number > 9
+        ? number
+        : '0' + number
+}
+
 export const UseEffectWithInterval = () => {
-    const [timer, setTimer] = useState<undefined | number>(undefined)
     const [date, setDate] = useState(new Date())
     useEffect(() => {
-        let id:number = +setInterval(() => {
+        console.log('clock')
+        setInterval(() => {
             setDate(new Date(Date.now()))
         }, 1000)
-        setTimer(id)
     }, [])
-    let hours = date.getHours() > 9? date.getHours(): '0' + date.getHours()
-    let minutes = date.getMinutes() > 9? date.getMinutes(): '0' + date.getMinutes()
-    let seconds = date.getSeconds() > 9? date.getSeconds(): '0' + date.getSeconds()
+    let hours = transfer(date.getHours())
+    let minutes = transfer(date.getMinutes())
+    let seconds = transfer(date.getSeconds())
     return (
         <div>
             <div>{hours}:{minutes}:{seconds}</div>
